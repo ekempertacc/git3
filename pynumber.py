bc15cc7de2c5266764f70def09be575bc37886a1
ekempertacc <ekemper@tacc.utexas.edu>
2025-06-16 16:19:28 -0500

    Update pynumber.py to 10

--- pynumber.py
+++ pynumber.py
@@ -1,6 +1,6 @@
 import random
 
-# Generate a random number between 1 and 5 (inclusive)
-random_number = random.randint(1, 6)
+# Generate a random number between 1 and 8 (inclusive)
+random_number = random.randint(1, 8)
 
 print("Random number:", random_number)
